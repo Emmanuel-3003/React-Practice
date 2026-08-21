5530ab2a154c1840ee6d9c5b398670991bb4999e
Emmanuel Gabriel <degabriels03@gmail.com>
2026-08-21 12:31:11 +0530

    miltiple api with axios

--- api-fetching/src/App.jsx
+++ api-fetching/src/App.jsx
@@ -6,16 +6,20 @@ function App() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const url = 'https://jsonplaceholder.typicode.com/posts';
+  const [data2, setData2] = useState([]);
+
+  const url1 = 'https://jsonplaceholder.typicode.com/posts';
+  const url2 = 'https://jsonplaceholder.typicode.com/users';
 
   useEffect(() => {
     setLoading(true);
-    axios(url)
-      .then(response => {
-        setData(response.data);
+    axios.all([axios.get(url1), axios.get(url2)])
+      .then(axios.spread((posts, users) => {
+        setData(posts.data);
+        setData2(users.data);
         setLoading(false);
         //throw new Error('An error occurred while fetching data');
-      })
+      }))
       .catch(error => {
         setError(error);
         setLoading(false);
@@ -33,11 +37,24 @@ function App() {
   return (
     <div>
       <h1>API Fetching...</h1>
+
+      <h2>Posts</h2>
       <ul>
-        {data.map ((item) => (
-          <li key = {item.id}>
-            <strong>{item.title}</strong>
-            <p>{item.body}</p>
+        {data.map ((post) => (
+          <li key = {post.id}>
+            <strong>{post.title}</strong>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+
+      <br />
+      <h2>Users</h2>
+      <ul>
+        {data2.map ((user) => (
+          <li key = {user.id}>
+            <strong>{user.name}</strong>
+            <p>{user.email}</p>
           </li>
         ))}
       </ul>

365dbc779f60709905db85e2a57813ad5960771e
Emmanuel Gabriel <degabriels03@gmail.com>
2026-08-25 15:50:30 +0530

    minor changes

--- react-tailwind/src/App.jsx
+++ react-tailwind/src/App.jsx
@@ -5,10 +5,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline italic">
-        Hello world!
-      </h1>
+    <div className='bg-gray-400 flex justify-center items-center h-screen'>
+      <div className = "bg-white p-8 rounded-lg shadow-2xl">
+        <h1 class="text-3xl text-gray-700 font-bold underline italic">
+          Hello, Tailwind CSS in React!!
+        </h1>
+        <p className='text-2xl text-yellow-500 italic'>This is a simple example of Tailwind CSS in a React project..!!</p>
+    </div>
     </div>
   )
 }
